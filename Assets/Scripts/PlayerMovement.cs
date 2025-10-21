74c311802d58d6816fa628b5279e8b0bc904baf5
Adam <132231549+CaptainAdamius@users.noreply.github.com>
2025-10-21 21:51:46 +1300

    Revamped game idea
    WE'RE MAKING TOUHOU NOW I GUESS
    - vertical movement
    - basic ui
    - pixel art & boundaries

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,9 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed;
+    public float screenLeft, screenRight, screenTop, screenBottom;
     float horizontal;
+    float vertical;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,9 +16,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
-        Vector3 moveDirection = new Vector2 (horizontal, 0);
+        Vector3 moveDirection = new Vector2 (horizontal, vertical);
         transform.position += moveDirection * Time.deltaTime * speed;
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, screenLeft, screenRight);
+        pos.y = Mathf.Clamp(pos.y, screenBottom, screenTop);
+        transform.position = pos;
     }
 }
